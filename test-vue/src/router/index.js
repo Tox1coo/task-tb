ea548055d85d0941f738bed54e3fcd8e5dc32a14
Daniil <www.daniil_.ru@mail.ru>
2023-10-31 23:55:46 +0700

    last commit

--- test-vue/src/router/index.js
+++ test-vue/src/router/index.js
@@ -6,12 +6,9 @@ const routes = [
     { path: '/', component: Home },
     { path: '/test', component: Test },
 ]
-// 3. Create the router instance and pass the `routes` option
-// You can pass in additional options here, but let's
-// keep it simple for now.
+
 export const router = createRouter({
-    // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
     history: createWebHistory(),
-    routes, // short for `routes: routes`
+    routes,
 })
 
